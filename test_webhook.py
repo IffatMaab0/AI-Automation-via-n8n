a8dfed52d9505bed08a48596fa365d8fd3f26998
IffatMaab0 <iffatmaab11@gmail.com>
2026-04-14 18:15:32 +0500

    Modified the output

--- test_webhook.py
+++ test_webhook.py
@@ -1,7 +1,17 @@
 import requests
 
-user_msg = "what's up"
+user_msg = "tell me a joke"
 req_msg = {"message": user_msg }
+
+
+# localhost (my PC)
+#    ↓
+# n8n runs here
+#    ↓
+# ngrok tunnels it
+#    ↓
+# public URL (internet access)
+
 
 url = "https://squeegee-repulsion-browsing.ngrok-free.dev/webhook-test/75d82094-071a-42cb-bcf0-97a76871ab2a"
 
@@ -9,3 +19,4 @@ url = "https://squeegee-repulsion-browsing.ngrok-free.dev/webhook-test/75d82094-
 response = requests.post(url, json =req_msg)
 
 print(response.status_code)
+print(response.json()[0]["output"])
